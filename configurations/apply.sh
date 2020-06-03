3515ef95256fb02e0b91993fbe584ba6d2b905ca
Sergey Fedyanin <vesalink@gmail.com>
2020-06-03 09:36:31 -0700

    some config changes / refactor

--- configurations/apply.sh
+++ configurations/apply.sh
@@ -4,35 +4,35 @@ set -Eeuo pipefail
 
 source .env
 # The following variables must be set in the .env file
-: "$ES_URL"
-: "$ES_USERNAME"
-: "$ES_PASSWORD"
+: "$ELASTICSEARCH_URL"
+: "$ELASTICSEARCH_USERNAME"
+: "$ELASTICSEARCH_PASSWORD"
 : "$KIBANA_URL"
 
-#curl -kSsL --user "${ES_USERNAME}:${ES_PASSWORD}" \
+#curl -kSsL --user "${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD}" \
 #    -XGET -H 'Content-Type: application/json' \
-#    "${ES_URL}/_cluster/health?pretty" -o /dev/null
+#    "${ELASTICSEARCH_URL}/_cluster/health?pretty" -o /dev/null
 
-curl -kSsL --user "${ES_USERNAME}:${ES_PASSWORD}" -w "\n" \
+curl -kSsL --user "${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD}" -w "\n" \
     -X PUT -H 'Content-Type: application/json' --data-binary @api-index-template.json \
-    "${ES_URL}/_template/logs-access"
+    "${ELASTICSEARCH_URL}/_template/logs-access"
 
 
-curl -kSsL --user "${ES_USERNAME}:${ES_PASSWORD}" -w "\n" \
+curl -kSsL --user "${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD}" -w "\n" \
     -X PUT -H 'Content-Type: application/json' --data-binary @api-pipeline.json \
-    "${ES_URL}/_ingest/pipeline/filebeat-7.6.2-apache-access-default"
+    "${ELASTICSEARCH_URL}/_ingest/pipeline/filebeat-7.7.0-apache-access-default"
 
 #set -x
-#curl  -kSsL -u "${ES_USERNAME}:${ES_PASSWORD}" \
+#curl  -kSsL -u "${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD}" \
 #      -X GET -H "Content-Type: application/json" -H "kbn-xsrf:true" \
 #      "${KIBANA_URL}/api/saved_objects/index-pattern/logs-access-*" | jq -M '.' > api-index-pattern.json
 echo "POST"
-curl  -kSsL -u "${ES_USERNAME}:${ES_PASSWORD}" \
+curl  -kSsL -u "${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD}" \
       -X POST -H "Content-Type: application/json" -H "kbn-xsrf:true" --data-binary @api-index-pattern.json \
       "${KIBANA_URL}/api/saved_objects/index-pattern/logs-access-*" -o /dev/null
 
 echo "PUT"
-curl  -kSsL -u "${ES_USERNAME}:${ES_PASSWORD}" \
+curl  -kSsL -u "${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD}" \
       -X PUT -H "Content-Type: application/json" -H "kbn-xsrf:true" --data-binary @api-index-pattern.json \
       "${KIBANA_URL}/api/saved_objects/index-pattern/logs-access-*" -o /dev/null
 
