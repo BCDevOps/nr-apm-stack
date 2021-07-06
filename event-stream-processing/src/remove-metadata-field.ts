360da33e710e00e13ad1b3a0fb8455abe8bc0d10
Clécio Varjão <1348549+cvarjao@users.noreply.github.com>
2021-07-05 17:25:36 -0700

    Added remove metadata field

---
+++ event-stream-processing/src/remove-metadata-field.ts
@@ -0,0 +1,14 @@
+import { injectable } from "inversify";
+import { Parser } from "./parser.isvc";
+import * as lodash from 'lodash'
+
+
+@injectable()
+export class RemoveMetadataField implements Parser  {
+    matches(record: any): boolean {
+        return !lodash.isNil(record['@metadata']);
+    }   
+    apply(record: any): void {
+        delete record['@metadata']
+    }
+}
