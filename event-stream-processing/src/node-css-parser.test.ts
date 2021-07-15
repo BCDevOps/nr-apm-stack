f7fa3d3a5ed3cff61a3205a98dd5c268499945f5
Csaky <Tim.Csaky@gov.bc.ca>
2021-07-15 13:28:30 -0700

    mprove query params and ip address formatting

--- event-stream-processing/src/node-css-parser.test.ts
+++ event-stream-processing/src/node-css-parser.test.ts
@@ -1,50 +1,7 @@
-import { NodeCssParser } from './node-css-parser';
+import {NodeCssParser} from './node-css-parser';
 
-test('basic', () => {
+test('basic - GET request', () => {
   const filter = new NodeCssParser();
-
-  /* eslint-disable */
-
-  // record from fluent-bit looks like this:
-  /*
-
-  [
-    1
-  ] app.http: [
-    1626304625.036902482,
-    {
-      "statusCode"=>200,
-      "userAgent"=>"Mozilla/5.0",
-      "path"=>"/api/v1/forms",
-      "message"=>"GET /api/v1/forms 200 624ms",
-      "logFilePath"=>"/var/log/app.log",
-      "logFileOffset"=>759,
-      "hostname"=>"chefs-app-123",
-      "timestamp"=>"2021-07-14T23:17:05.036Z",
-      "product"=>"chefs",
-      "namespace"=>"123-dev",
-      "azp"=>"chefs-frontend",
-      "level"=>"http",
-      "contentLength"=>"323",
-      "agent"=>{
-        "type"=>"fluentbit"
-      },
-      "responseTime"=>624,
-      "httpVersion"=>"1.1",
-      "event"=>{
-        "type"=>"http",
-        "module"=>"node.css",
-        "kind"=>"event",
-        "dataset"=>"node.css.http"
-      },
-      "ip"=>"::ffff:10.97.8.1",
-      "log"=>"{"responseTime":624,"azp":"chefs-frontend","contentLength":"323","httpVersion":"1.1","ip":": :ffff: 10.97.8.1","method":"GET","path":"/api/v1/forms","statusCode":200,"userAgent":"Mozilla/5.0","level":"http","message":"GET /api/v1/forms 200 624ms","timestamp":"2021-07-14T23: 17: 05.036Z"}",
-      "method"=>"GET"
-    }
-  ]
-  */
-  /* eslint-enable */
-
 
   // --- the test record object for parsing
   const record = {
@@ -52,6 +9,7 @@ test('basic', () => {
       'type': 'fluentbit',
     },
     'azp': 'chefs-frontend',
+    'clientIp': '2001:db8:3333:4444:5555:6666:7777:8888',
     'contentLength': '323',
     'event': {
       'type': 'http',
@@ -61,28 +19,38 @@ test('basic', () => {
     },
     'hostname': 'chefs-app-123',
     'httpVersion': '1.1',
-    'ip': '::ffff:10.97.8.1',
+    'hostIp': '10.97.8.1',
     'level': 'http',
     'log': {
       'responseTime': 624,
       'azp': 'chefs-frontend',
+      'clientIp': '2001:db8:3333:4444:5555:6666:7777:8888',
       'contentLength': '323',
       'httpVersion': '1.1',
-      'ip': ': :ffff: 10.97.8.1',
+      'hostIp': '::ffff: 10.97.8.1',
       'method': 'GET',
       'path': '/api/v1/forms',
+      'query': {
+        'active': 'true',
+        'deleted': 'false',
+      },
       'statusCode': 200,
       'userAgent': 'Mozilla/5.0',
       'level': 'http',
-      'message': 'GET /api/v1/forms 200 624ms', 'timestamp': '2021-07-14T23: 17: 05.036Z',
+      'message': 'GET /api/v1/forms?active=true 200 624ms', 'timestamp': '2021-07-14T23: 17: 05.036Z',
     },
     'logFilePath': '/var/log/app.log',
     'logFileOffset': 759,
+    'logStreamDate': '2021-07-15T20:08:48.845207Z',
     'message': 'GET /api/v1/forms 200 624ms',
     'method': 'GET',
     'namespace': '123-dev',
     'path': '/api/v1/forms',
     'product': 'chefs',
+    'query': {
+      'active': 'true',
+      'deleted': 'false',
+    },
     'responseTime': 624,
     'statusCode': 200,
     'timestamp': '2021-07-14T23:17:05.036Z',
@@ -96,6 +64,9 @@ test('basic', () => {
   filter.apply(record);
 
   // --- test the result:
+  // clientIp
+  expect(record).not.toHaveProperty('clientIp');
+  expect(record).toHaveProperty('client.ip', '2001:db8:3333:4444:5555:6666:7777:8888');
   // contentLength
   expect(record).not.toHaveProperty('contentLength');
   expect(record).toHaveProperty('http.response.body.bytes', '323');
@@ -105,9 +76,9 @@ test('basic', () => {
   // httpVersion
   expect(record).not.toHaveProperty('httpVersion');
   expect(record).toHaveProperty('http.version', '1.1');
-  // ip
-  expect(record).not.toHaveProperty('ip');
-  expect(record).toHaveProperty('source.address', '10.97.8.1');
+  // hostIp
+  expect(record).not.toHaveProperty('hostIp');
+  expect(record).toHaveProperty('host.ip', '10.97.8.1');
   // log
   expect(record).toHaveProperty('event.original.responseTime', 624);
   // logFileOffset
@@ -124,7 +95,10 @@ test('basic', () => {
   expect(record).toHaveProperty('host.namespace', '123-dev');
   // path
   expect(record).not.toHaveProperty('path');
-  expect(record).toHaveProperty('url.original', '/api/v1/forms');
+  expect(record).toHaveProperty('url.path', '/api/v1/forms');
+  // query
+  expect(record).not.toHaveProperty('query');
+  expect(record).toHaveProperty('url.query', 'active=true&deleted=false');
   // responseTime
   expect(record).not.toHaveProperty('responseTime');
   expect(record).toHaveProperty('http.response.time', 624);
@@ -140,9 +114,8 @@ test('basic', () => {
 
   // fields already in ECS format:
   // message
-
   // additional fields:
-  // azp, level, product
+  // azp, level, logStreamDate, product
 
   // use snapshot for test
   expect(record).toMatchSnapshot('2f015294-45f8-4235-949b-667236bea641');
--- event-stream-processing/src/node-css-parser.ts
+++ event-stream-processing/src/node-css-parser.ts
@@ -1,10 +1,25 @@
-import { injectable } from 'inversify';
-import { Parser } from './parser.isvc';
+import {injectable} from 'inversify';
+import {Parser} from './parser.isvc';
 import * as lodash from 'lodash';
-import { renameField } from './shared/rename-field';
+import {renameField} from './shared/rename-field';
 
+/**
+* This class can parse logs that are received in a JOSN format
+* to make them more suited to indexing in a log database
+* This class has been used to parse logs from a NodeJS/Express web application
+* output with an Express Winston logger (https://www.npmjs.com/package/express-winston)
+* and forwarded from the application by Fluent-bit
+* @author The Common Services Showcase team <NR.CommonServiceShowcase@gov.bc.ca>
+* @author Tim Csaky <tim.csaky@gov.bc.ca>
+* @summary A parser for converting application logs into an ECS compatible format
+*/
 @injectable()
 export class NodeCssParser implements Parser {
+  /**
+   * Checks various properties of the incoming log (eg: event.kind and event.dataset)
+   * to determine if it should be parsed by this parser
+   * @param {any} record
+   */
   matches(record: any): boolean {
     return record.event?.kind === 'event' &&
       // only parse 'http' (access) logs for now.
@@ -12,20 +27,30 @@ export class NodeCssParser implements Parser {
       // lodash.includes(record?.event.dataset, 'node.css');
       lodash.includes(record?.event.dataset, 'node.css.http');
   }
+  /**
+   * Applies formatting changes to the incoming log
+   * to make it match the ELastic Common Schema
+   * see: https://www.elastic.co/guide/en/ecs/current/ecs-reference.html#_what_is_ecs
+   * @param {any} record
+   */
   apply(record: any): void {
     // parse logs from Fluent-bit
     if (lodash.get(record, 'agent.type', '') === 'fluentbit') {
-      renameField(record, 'log', 'event.original');
+      record.clientIp = this.chooseIpvFormat(record.clientIp);
+      renameField(record, 'clientIp', 'client.ip');
       renameField(record, 'contentLength', 'http.response.body.bytes');
       renameField(record, 'hostname', 'host.hostname');
       renameField(record, 'httpVersion', 'http.version');
-      record.ip = this.formatSourceIp(record.ip);
-      renameField(record, 'ip', 'source.address');
+      record.hostIp = this.chooseIpvFormat(record.hostIp);
+      renameField(record, 'hostIp', 'host.ip');
+      renameField(record, 'log', 'event.original');
       renameField(record, 'logFileOffset', 'log.file.offset');
       renameField(record, 'logFilePath', 'log.file.path');
       renameField(record, 'method', 'http.request.method');
       renameField(record, 'namespace', 'host.namespace');
-      renameField(record, 'path', 'url.original');
+      renameField(record, 'path', 'url.path');
+      record.query = this.showQueryParamsAsString(record.query);
+      renameField(record, 'query', 'url.query');
       renameField(record, 'responseTime', 'http.response.time');
       renameField(record, 'statusCode', 'http.response.status_code');
       renameField(record, 'timestamp', '@timestamp');
@@ -34,11 +59,26 @@ export class NodeCssParser implements Parser {
   }
 
   /**
- * Remove user from incoming ip
- * eg: '::ffff:10.97.6.1' >>> '10.97.6.1'
+ * NodeJS app is outputting the record.hostIp as an IPV6 formatted ip address.
+ * eg: '::ffff:10.97.6.1'.  * '::ffff:' is a designated as the IPv4 to IPv6 subnet prefix
+ * to show IPv4 addresses in an IPV6 format.
+ * see: https://whatismyipaddress.com/work-arounds
+ * In an effort to unify ECS ip addresses we can revert to a regular IPV4 address
+ * (eg: '10.97.6.1') if it contains this mechanism
  * @param {string} value - the original record.ip field
  */
-  formatSourceIp(value: string) {
-    return value.replace(/ /g, '').split(':').slice(-1).pop();
+  chooseIpvFormat(value: string) {
+    return value.startsWith('::ffff:') ? value.replace(/ /g, '').split(':').slice(-1).pop() : value;
+  }
+
+  /**
+   * serialize query paramaters
+   * from object properties back to a query string
+   */
+  showQueryParamsAsString(query: any) {
+    const queryString = Object.keys(query)
+      .map((key) => `${key}=${query[key]}`)
+      .join('&');
+    return queryString;
   }
 }
