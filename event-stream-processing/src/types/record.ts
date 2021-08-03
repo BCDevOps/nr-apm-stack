c967409924c9844e80198a21d8a4044e38b97a0e
Clécio Varjão <1348549+cvarjao@users.noreply.github.com>
2021-08-03 10:01:01 -0700

    feat: Add typedoc generation support (#54)
    * feat: Add typedoc generation support

---
+++ event-stream-processing/src/types/record.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+declare namespace ecs {
+    export type Record = {
+        /**
+         * This is the Elastic Search document _id and it is used for performing upserts which enables deduplication
+         * @see {@link https://www.elastic.co/guide/en/elasticsearch/reference/current/mapping-id-field.html}
+         */
+        _id: string;
+        /**
+         * Date/time when the event originated.
+         * @see {@link https://www.elastic.co/guide/en/ecs/current/ecs-base.html#field-timestamp}
+         */
+        '@timestamp': string;
+        /**
+         * For log events the message field contains the log message, optimized for viewing in a log viewer.
+         * @see {@link https://www.elastic.co/guide/en/ecs/current/ecs-base.html#field-message}
+         */
+        message: string;
+        /**
+         * @deprecated This attribute has moved to {@link Log}
+         */
+        offset?: number;
+        event: Event;
+        log?: Log;
+        host?: Host
+    }
+}
