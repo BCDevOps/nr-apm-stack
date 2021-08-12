5691a86107953ba12c330b0d7f9b0b578b4bc04d
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2021-08-12 14:30:53 -0700

    Feature/oneteam 1548 (#65)
    * ONETEAM-1548 Increase hot nodes to 6
    
    * ONETEAM-1548 Update data node instance type
    
    * ONETEAM-1548 Match platform changes to retention in days

--- terraform/main.tf
+++ terraform/main.tf
@@ -225,10 +225,12 @@ resource "aws_iam_service_linked_role" "es" {
 
 resource "aws_cloudwatch_log_group" "es_application_logs" {
   name = "/aws/aes/domains/${ local.es_domain_name }-${ time_static.log_group_suffix.unix }/application-logs"
+  retention_in_days = 90
 }
 
 resource "aws_cloudwatch_log_group" "es_audit_logs" {
   name = "/aws/aes/domains/${ local.es_domain_name }-${ time_static.log_group_suffix.unix }/audit-logs"
+  retention_in_days = 90
 }
 
 resource "aws_cloudwatch_log_resource_policy" "es_logs" {
