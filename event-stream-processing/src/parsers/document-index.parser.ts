f4c23cc1b5097b26cdcffa074da4c005cacc6222
Andreas Wilson <41663792+andrwils@users.noreply.github.com>
2021-12-01 11:40:12 -0800

    refactor document index parser for data field sub

--- event-stream-processing/src/parsers/document-index.parser.ts
+++ event-stream-processing/src/parsers/document-index.parser.ts
@@ -3,7 +3,6 @@ import {Parser} from '../types/parser';
 import lodash from 'lodash';
 import moment from 'moment';
 import {OsDocument} from '../types/os-document';
-
 @injectable()
 /**
  * Apply index to document
@@ -19,40 +18,41 @@ export class DocumentIndexParser implements Parser {
   matches(document: OsDocument): boolean {
     return !!(document.data['@metadata'] && document.data['@metadata'].index);
   }
-
   /**
    * Apply index to document
    * @param document The document to modify
    */
   apply(document: OsDocument): void {
-    const timestamp = lodash.get(document.data, '@timestamp');
-    const app: string = lodash.get(document.data, 'labels.application');
-    const index: string = lodash.get(document.data, '@metadata.index');
-    if (!index) {
+    let indexName: string = lodash.get(document.data, '@metadata.index');
+    if (!indexName) {
       throw new Error('Could not map event to an index');
     }
+    indexName = this.applyTimestampSubsitution(document, indexName);
+    indexName = this.applyDataFieldSubsitution(document, indexName);
+    document.index = indexName;
+  }
+  private applyTimestampSubsitution(document: OsDocument, index: string): string {
+    const timestamp = lodash.get(document.data, '@timestamp');
     if (lodash.isNil(timestamp)) {
       throw new Error('@timestamp field value has not been defined');
     }
     const tsMomement = moment(timestamp);
-    if (!app) {
-      const indexFormat: string = index.replace(/\<\%\=[^\=]+=\%\>/gm, (match: string) => {
-        if (match.startsWith('<%=')) {
-          return tsMomement.format(match.substring(3, match.length - 3));
-        }
-        throw new Error(`Unexpected formatting: ${match}`);
-      });
-      document.index = indexFormat;
+    return index.replace(/\<\%\=[^\=]+=\%\>/gm, (match: string) => {
+      if (match.startsWith('<%=')) {
+        return tsMomement.format(match.substring(3, match.length - 3));
+      }
+      throw new Error(`Unexpected formatting: ${match}`);
+    });
+  }
+  private applyDataFieldSubsitution(document: OsDocument, index: string): string {
+    const indexDataFieldSubstitute: string = lodash.get(document.data, '@metadata.indexDataFieldSubstitute');
+    if (indexDataFieldSubstitute) {
+      const documentDataField: string = lodash.get(document.data, indexDataFieldSubstitute);
+      if (lodash.isNil(documentDataField)) {
+        throw new Error(`${indexDataFieldSubstitute} field value not in document`);
+      }
+      return index.replace(/\<!\=[^\=]+=!\>/gm, documentDataField);
     }
-    if (app) {
-      const indexFormatWithApp = index.replace(/\<!\=[^\=]+=!\>/gm, app);
-      const indexFormat: string = indexFormatWithApp.replace(/\<\%\=[^\=]+=\%\>/gm, (match: string) => {
-        if (match.startsWith('<%=')) {
-          return tsMomement.format(match.substring(3, match.length - 3));
-        }
-        throw new Error(`Unexpected formatting: ${match}`);
-      });
-      document.index = indexFormat;
-    }
+    return index;
   }
 }
