1c4b3ef365e0b6b21871a60a82bb1d3a513fdaf3
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2021-11-30 16:11:14 -0800

    feat: Log parse errors

---
+++ event-stream-processing/src/util/parser.error.ts
@@ -0,0 +1,8 @@
+import {GenericError} from './generic.error';
+
+export class ParserError extends GenericError {
+  constructor(message: string, public parser: string, source?: Error | undefined) {
+    super(message, source);
+    Object.setPrototypeOf(this, new.target.prototype);
+  }
+}
