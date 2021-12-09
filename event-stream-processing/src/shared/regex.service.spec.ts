4e4345cb9dec535f8c52a572804cf9f4c7fedce0
Andreas Wilson <41663792+andrwils@users.noreply.github.com>
2021-12-08 17:07:17 -0800

    fixed unit test

--- event-stream-processing/src/shared/regex.service.spec.ts
+++ event-stream-processing/src/shared/regex.service.spec.ts
@@ -31,8 +31,8 @@ describe('RegexService', () => {
   it('parses suspicious entry - 001', () => {
     const parser = new RegexService(logger);
     // eslint-disable-next-line max-len
-    const document = {data: JSON.parse(JSON.stringify({...APACHE_ACCESS_LOG_EVENT_SIGNATURE, message: 'v1.0 20120211 "http://142.34.120.18:80" "64.114.237.115" [25/May/2021:15:53:10 -0700] "GET /WebID/IISWebAgentIF.dll?postdata=\\"><script>foo</script> HTTP/1.1" 302 347 bytes 1152 bytes "-" "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0)" 1 ms, "-" "-"'}))} as unknown as OsDocument;
-    parser.applyRegex(document, 'message', regexArr);
+    const document = {data: JSON.parse(JSON.stringify({...APACHE_ACCESS_LOG_EVENT_SIGNATURE, 'event.original': 'v1.0 20120211 "http://142.34.120.18:80" "64.114.237.115" [25/May/2021:15:53:10 -0700] "GET /WebID/IISWebAgentIF.dll?postdata=\\"><script>foo</script> HTTP/1.1" 302 347 bytes 1152 bytes "-" "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0)" 1 ms, "-" "-"'}))} as unknown as OsDocument;
+    parser.applyRegex(document, 'event.original', regexArr);
     // eslint-disable-next-line max-len
     expect(document.data).toHaveProperty('http.request.line', 'GET /WebID/IISWebAgentIF.dll?postdata=\\"><script>foo</script> HTTP/1.1');
     expect(document.data).toHaveProperty('event.duration', '1');
@@ -41,8 +41,8 @@ describe('RegexService', () => {
   it('parses suspicious entry - 002', () => {
     const parser = new RegexService(logger);
     // eslint-disable-next-line max-len
-    const document = {data: JSON.parse(JSON.stringify({...APACHE_ACCESS_LOG_EVENT_SIGNATURE, message: 'v1.0 20120211 "http://142.34.120.21:80" "64.114.237.115" [25/May/2021:15:51:47 -0700] "GET /cgi-bin/count.cgi HTTP/1.1" 302 369 bytes 1043 bytes "-" "() { _; } >_[$($())] { echo Content-Type: text/plain ; echo ; echo \\"bash_cve_2014_6278 Output : $((10+12))\\"; }" 1 ms, "-" "-"'}))} as unknown as OsDocument;
-    parser.applyRegex(document, 'message', regexArr);
+    const document = {data: JSON.parse(JSON.stringify({...APACHE_ACCESS_LOG_EVENT_SIGNATURE, 'event.original': 'v1.0 20120211 "http://142.34.120.21:80" "64.114.237.115" [25/May/2021:15:51:47 -0700] "GET /cgi-bin/count.cgi HTTP/1.1" 302 369 bytes 1043 bytes "-" "() { _; } >_[$($())] { echo Content-Type: text/plain ; echo ; echo \\"bash_cve_2014_6278 Output : $((10+12))\\"; }" 1 ms, "-" "-"'}))} as unknown as OsDocument;
+    parser.applyRegex(document, 'event.original', regexArr);
     expect(document.data).toHaveProperty('http.request.line', 'GET /cgi-bin/count.cgi HTTP/1.1');
     expect(document.data).not.toHaveProperty('http.request.referrer.original');
     // eslint-disable-next-line max-len
@@ -53,8 +53,8 @@ describe('RegexService', () => {
   it('parses Apache standard format', () => {
     const parser = new RegexService(logger);
     // eslint-disable-next-line max-len
-    const document = {data: JSON.parse(JSON.stringify({...APACHE_ACCESS_LOG_EVENT_SIGNATURE, message: '5.217.73.168 - - [05/Jun/2020:00:06:57 -0700] "GET /pub/eirs/viewDocumentDetail.do?fromStatic=true&repository=BDP&documentId=6612 HTTP/1.1" 200 277 "-" "AHC/2.1" 192'}))} as unknown as OsDocument;
-    parser.applyRegex(document, 'message', regexArr);
+    const document = {data: JSON.parse(JSON.stringify({...APACHE_ACCESS_LOG_EVENT_SIGNATURE, 'event.original': '5.217.73.168 - - [05/Jun/2020:00:06:57 -0700] "GET /pub/eirs/viewDocumentDetail.do?fromStatic=true&repository=BDP&documentId=6612 HTTP/1.1" 200 277 "-" "AHC/2.1" 192'}))} as unknown as OsDocument;
+    parser.applyRegex(document, 'event.original', regexArr);
     expect(document.data).toHaveProperty('source.ip', '5.217.73.168');
     // eslint-disable-next-line max-len
     expect(document.data).toHaveProperty('http.request.line', 'GET /pub/eirs/viewDocumentDetail.do?fromStatic=true&repository=BDP&documentId=6612 HTTP/1.1');
@@ -68,8 +68,8 @@ describe('RegexService', () => {
   it('parses Apache standard format - 2', () => {
     const parser = new RegexService(logger);
     // eslint-disable-next-line max-len
-    const document = {data: JSON.parse(JSON.stringify({...APACHE_ACCESS_LOG_EVENT_SIGNATURE, message: '66.183.191.120 - - [05/Jun/2020:07:23:23 -0700] "GET /ext/raad3/map?execution=e1s1&_eventId=agreed HTTP/1.1" 302 - "https://apps.nrs.gov.bc.ca/ext/raad3/map?execution=e1s1" "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0" 24'}))} as unknown as OsDocument;
-    parser.applyRegex(document, 'message', regexArr);
+    const document = {data: JSON.parse(JSON.stringify({...APACHE_ACCESS_LOG_EVENT_SIGNATURE, 'event.original': '66.183.191.120 - - [05/Jun/2020:07:23:23 -0700] "GET /ext/raad3/map?execution=e1s1&_eventId=agreed HTTP/1.1" 302 - "https://apps.nrs.gov.bc.ca/ext/raad3/map?execution=e1s1" "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0" 24'}))} as unknown as OsDocument;
+    parser.applyRegex(document, 'event.original', regexArr);
     expect(document.data).not.toHaveProperty('user.name');
     expect(document.data).toHaveProperty('source.ip', '66.183.191.120');
     expect(document.data).toHaveProperty(
@@ -86,8 +86,8 @@ describe('RegexService', () => {
   it('keeps dash - 3', () => {
     const parser = new RegexService(logger);
     // eslint-disable-next-line max-len
-    const document = {data: JSON.parse(JSON.stringify({...APACHE_ACCESS_LOG_EVENT_SIGNATURE, message: '66.183.191.120 - - [05/Jun/2020:07:23:23 -0700] "GET /ext/raad3/map?execution=e1s1&_eventId=agreed HTTP/1.1" 302 - "https://apps.nrs.gov.bc.ca/ext/raad3/map?execution=e1s1" "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0" 24'}))} as unknown as OsDocument;
-    parser.applyRegex(document, 'message', regexArr, false);
+    const document = {data: JSON.parse(JSON.stringify({...APACHE_ACCESS_LOG_EVENT_SIGNATURE, 'event.original': '66.183.191.120 - - [05/Jun/2020:07:23:23 -0700] "GET /ext/raad3/map?execution=e1s1&_eventId=agreed HTTP/1.1" 302 - "https://apps.nrs.gov.bc.ca/ext/raad3/map?execution=e1s1" "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0" 24'}))} as unknown as OsDocument;
+    parser.applyRegex(document, 'event.original', regexArr, false);
     expect(document.data).toHaveProperty('user.name', '-');
   });
 });
