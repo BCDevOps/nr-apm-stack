6521c31d43a021fbd62b496128f708024cafdf5b
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2021-12-29 14:05:05 -0800

    feat: Add disk usage sample

--- event-stream-processing/samples/metrics-conf/filters.lua
+++ event-stream-processing/samples/metrics-conf/filters.lua
@@ -16,6 +16,12 @@ function remove_nil_fields(tag, timestamp, record)
     return 2, timestamp, record
 end
 
+function copy(obj)
+  if type(obj) ~= 'table' then return obj end
+  local res = {}
+  for k, v in pairs(obj) do res[copy(k)] = copy(v) end
+  return res
+end
 
 -- Small stats library                      --
 ----------------------------------------------
@@ -204,4 +210,21 @@ function modify_cpu_stats(tag, timestamp, record)
         new_record["host.cpu.usage_core_stddev"] = stats.standardDeviation(core_cpu)
     end
     return 2, timestamp, new_record
+end
+
+function multiplex_disk_stats(tag, timestamp, record)
+  new_record = {}
+
+  for k, v in pairs(record["disk"]) do
+      new_record[k] = copy(record)
+      new_record[k]["host.disk.mount"] = v["mount"]
+      new_record[k]["host.disk.total"] = tonumber(v["total"])
+      new_record[k]["host.disk.free"] = tonumber(v["available"])
+      new_record[k]["host.disk.used"] = tonumber(v["total"]) - tonumber(v["available"])
+      new_record[k]["host.disk.used_percentage"] = new_record[k]["host.disk.used"] / tonumber(v["total"])
+
+      new_record[k]["disk"] = nil
+  end
+
+  return 2, timestamp, new_record
 end
