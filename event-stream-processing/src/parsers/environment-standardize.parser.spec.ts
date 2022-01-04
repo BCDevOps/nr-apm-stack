a02f06aa39135fefe4976466baed92e5c0b1c069
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2022-01-04 13:22:29 -0800

    feat: Add environment standardization

---
+++ event-stream-processing/src/parsers/environment-standardize.parser.spec.ts
@@ -0,0 +1,26 @@
+import {OsDocument} from '../types/os-document';
+import {EnvironmentStandardizeParser} from './environment-standardize.parser';
+
+describe('EnvironmentStandardizeParser', () => {
+  it('matches using metadata', () => {
+    const parser = new EnvironmentStandardizeParser();
+
+    expect(parser.matches({data: {'@metadata': {environmentStandardize: true}}} as unknown as OsDocument)).toBe(true);
+    expect(parser.matches({data: {'@metadata': {}}} as unknown as OsDocument)).toBe(false);
+  });
+
+  it('standardizes environment', () => {
+    const parser = new EnvironmentStandardizeParser();
+    const document = {
+      data: {'service': {environment: 'wfint'}, '@metadata': {environmentStandardize: true}},
+    } as unknown as OsDocument;
+    parser.apply(document);
+    expect(document.data.service).toEqual({
+      environment: 'integration',
+    });
+    expect(document.data.labels).toEqual({
+      env: 'integration',
+      env_alias: 'wfint',
+    });
+  });
+});
---
+++ event-stream-processing/src/parsers/environment-standardize.parser.ts
@@ -0,0 +1,49 @@
+import {injectable} from 'inversify';
+import {OsDocument} from '../types/os-document';
+import {Parser} from '../types/parser';
+import lodash from 'lodash';
+
+const envAlias: {[key: string]: string} = {
+  production: 'production',
+  test: 'test',
+  integration: 'development',
+  staging1: 'production',
+  staging2: 'production',
+  staging3: 'production',
+  wfprd: 'production',
+  wftst: 'test',
+  wfint: 'development',
+  wfdlv: 'development',
+};
+
+@injectable()
+/**
+ * Standardize service environment field
+ * @deprecated
+ */
+export class EnvironmentStandardizeParser implements Parser {
+  /**
+   * Returns true if metadata has a environmentStandardize field.
+   * @param document The document to match against
+   * @returns
+   */
+  matches(document: OsDocument): boolean {
+    return !!(document.data['@metadata'] && document.data['@metadata'].environmentStandardize);
+  }
+
+  /**
+   * Standardize service environment field
+   * @param document The document to modify
+   */
+  apply(document: OsDocument): void {
+    const value = lodash.get(document.data, 'service.environment');
+    if (value) {
+      const standardEnv = envAlias[value] ? envAlias[value] : 'unknown';
+      lodash.set(document.data, 'labels.env', standardEnv);
+      lodash.set(document.data, 'service.environment', standardEnv);
+      if (value !== standardEnv) {
+        lodash.set(document.data, 'labels.env_alias', value);
+      }
+    }
+  }
+}
