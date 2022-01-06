65432a59bf3351f8c67b4a9eb2950afed2df5e94
Matthew Bystedt <mbystedt@gmail.com>
2022-01-06 13:23:19 -0800

    fix: Add in a couple index actions csv download requires (#106)

--- terraform/main.tf
+++ terraform/main.tf
@@ -561,7 +561,7 @@ resource "elasticsearch_opendistro_role" "nrm_read_all" {
   ]
   index_permissions {
     index_patterns  = ["iitd-*", "iit-*", "nrm-*"]
-    allowed_actions = ["read", "indices:admin/resolve/index"]
+    allowed_actions = ["read", "indices:admin/resolve/index", "indices:data/read/get", "indices:monitor/settings/get"]
     masked_fields = ["source.ip", "client.ip"]
     field_level_security = ["~message", "~event.original", "~http.request.line"]
   }
@@ -630,7 +630,7 @@ resource "elasticsearch_opendistro_role" "nrm_security" {
   ]
   index_permissions {
     index_patterns  = ["iitd-*", "iit-*", "nrm-*"]
-    allowed_actions = ["read", "indices:admin/resolve/index"]
+    allowed_actions = ["read", "indices:admin/resolve/index", "indices:data/read/get", "indices:monitor/settings/get"]
   }
   index_permissions {
     index_patterns  = [".kibana_*"]
