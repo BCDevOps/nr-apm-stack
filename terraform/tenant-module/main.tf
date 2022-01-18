933ebfaefd6c75e15bc1b4992393f81a90bf8d3b
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2022-01-18 13:56:16 -0800

    fix: Add providers

--- terraform/tenant-module/main.tf
+++ terraform/tenant-module/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.63.0"
+    }
+    elasticsearch = {
+      source = "phillbaker/elasticsearch"
+      version = "1.6.3"
+    }
+  }
+}
+
 
 resource "elasticsearch_opendistro_role" "tenant_role" {
   role_name   = var.tenant.role_name
