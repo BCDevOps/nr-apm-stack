568091799275ef6088d94ed766aff040ee36f2de
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2022-02-01 14:04:25 -0800

    feat: Add back context and tweak ApplicationClassificationParser

--- event-stream-processing/src/parsers/application-classification.parser.ts
+++ event-stream-processing/src/parsers/application-classification.parser.ts
@@ -7,7 +7,7 @@ import * as querystring from 'querystring';
 import {OsDocument} from '../types/os-document';
 
 // eslint-disable-next-line max-len
-const knownAppContextRegex = /^(?<labels__context>\/((int)|(ext)|(pub)|(gov)|(datasets)|(appsdata))(\/((geoserver)|(pls)))?)\/(?<labels__application>[^\/]*).*$/m;
+const knownAppContextRegex = /^(?<labels__context>\/((int)|(ext)|(pub)|(gov)|(datasets)|(appsdata))(\/((geoserver)|(pls)))?)\/(?<labels__project>[^\/]*).*$/m;
 
 @injectable()
 /**
