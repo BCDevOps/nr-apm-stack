2220e08d7211e5960bbbe6f90e803d88372448f0
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2022-03-08 16:52:32 -0800

    fix: Increase body parser limit (#138)

--- event-stream-processing/src/main.ts
+++ event-stream-processing/src/main.ts
@@ -1,12 +1,13 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './nest/app.module';
-
+import {json} from 'body-parser';
 /**
  * Bootstrap the nest application for localhost testing.
  */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.getHttpAdapter().getInstance().set('json spaces', 2);
+  app.use(json({limit: '50mb'}));
   await app.listen(3000);
 }
 void bootstrap();
