e68fa077fd490bafd6760067795559dc620c422e
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2022-03-18 15:53:10 -0700

    fix: message permissions (#140)

--- terraform/main.tf
+++ terraform/main.tf
@@ -560,7 +560,7 @@ resource "elasticsearch_opendistro_role" "nrm_read_all" {
     index_patterns  = ["iitd-*", "iit-*", "nrm-*"]
     allowed_actions = ["read", "indices:admin/resolve/index", "indices:data/read/get", "indices:monitor/settings/get"]
     masked_fields = ["source.ip", "client.ip"]
-    field_level_security = ["~message", "~event.original", "~http.request.line"]
+    field_level_security = ["~event.original", "~http.request.line"]
   }
   index_permissions {
     index_patterns  = [".kibana_*"]
