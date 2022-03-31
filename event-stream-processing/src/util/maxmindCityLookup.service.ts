4d7a47c4845760c5e5074b015d2d270c6b0e4784
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2022-03-31 09:10:11 -0700

    fix: spacing

--- event-stream-processing/src/util/maxmindCityLookup.service.ts
+++ event-stream-processing/src/util/maxmindCityLookup.service.ts
@@ -7,15 +7,15 @@ import * as fs from 'fs';
 import {MaxmindLookupService} from './maxmindLookup.service';
 @injectable()
 export class MaxmindCityLookupService implements MaxmindLookupService<CityResponse> {
-    private cityLookup: Reader<CityResponse> | null = null;
-    public constructor() {
-      const dbPath = process.env.MAXMIND_DB_DIR || path.join(__dirname, '../asset');
-      const filePath = path.join(dbPath, 'GeoLite2-City.mmdb');
-      if (fs.existsSync(filePath)) {
-        this.cityLookup = new Reader<CityResponse>(fs.readFileSync(filePath));
-      }
+  private cityLookup: Reader<CityResponse> | null = null;
+  public constructor() {
+    const dbPath = process.env.MAXMIND_DB_DIR || path.join(__dirname, '../asset');
+    const filePath = path.join(dbPath, 'GeoLite2-City.mmdb');
+    if (fs.existsSync(filePath)) {
+      this.cityLookup = new Reader<CityResponse>(fs.readFileSync(filePath));
     }
-    public lookup(ipAddress: string): CityResponse | null {
-      return this.cityLookup ? this.cityLookup.get(ipAddress) : null;
-    }
+  }
+  public lookup(ipAddress: string): CityResponse | null {
+    return this.cityLookup ? this.cityLookup.get(ipAddress) : null;
+  }
 }
