71a1f948e5c1758438f82b977cf4b0d2a36a3b96
GraceRuan <106621189+GraceRuan@users.noreply.github.com>
2022-10-19 09:06:59 -0700

    feat: upgrade ECS schema to 8.4 (#238)

--- workflow-cli/configuration-opensearch/fetch_ecs_component_template.sh
+++ workflow-cli/configuration-opensearch/fetch_ecs_component_template.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-ecs_verion=1.12
+ecs_verion=8.4
 
 curl -L -qq https://github.com/elastic/ecs/archive/refs/heads/${ecs_verion}.zip --output ecs.zip
 
-unzip -qq -j -o ecs.zip "ecs-${ecs_verion}/generated/elasticsearch/component/*" -d ecs_${ecs_verion}
+unzip -qq -j -o ecs.zip "ecs-${ecs_verion}/generated/elasticsearch/composable/component/*" -d ecs_${ecs_verion}
 
 rm ecs.zip
