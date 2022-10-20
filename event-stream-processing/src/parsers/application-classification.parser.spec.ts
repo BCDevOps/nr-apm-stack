5ebea8f400732decb79f757ede50368562228e29
GraceRuan <grace.ruan@gov.bc.ca>
2022-10-20 13:28:18 -0700

    fix: modify application-classification.parser.spec.ts

--- event-stream-processing/src/parsers/application-classification.parser.spec.ts
+++ event-stream-processing/src/parsers/application-classification.parser.spec.ts
@@ -1,9 +1,10 @@
 import {ApplicationClassificationParser} from './application-classification.parser';
 import {OsDocument} from '../types/os-document';
+import {RegexService} from '../shared/regex.service';
 
 describe('ParserApplicationClasification', () => {
   it('app - sitesandtrailsbc.ca', () => {
-    const parser = new ApplicationClassificationParser();
+    const parser = new ApplicationClassificationParser({} as unknown as RegexService);
     const document = {
       data: {url: {domain: 'www.del.sitesandtrailsbc.ca', path: '/resources/REC2164/siteimages/images.properties.txt'}},
     } as unknown as OsDocument;
@@ -13,7 +14,7 @@ describe('ParserApplicationClasification', () => {
   });
 
   it('app - clp-cgi', () => {
-    const parser = new ApplicationClassificationParser();
+    const parser = new ApplicationClassificationParser({} as unknown as RegexService);
     const document = {
       data: {url: {domain: '142.34.120.12', path: '/clp-cgi/accessDenied.cgi'}},
     } as unknown as OsDocument;
