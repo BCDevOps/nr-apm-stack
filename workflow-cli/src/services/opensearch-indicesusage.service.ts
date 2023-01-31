a3fc279115a19d57c5dbc1fc9cafb9f066981f31
GraceRuan <grace.ruan@gov.bc.ca>
2023-01-31 15:45:43 -0800

    fix: add aggregration query

--- workflow-cli/src/services/opensearch-indicesusage.service.ts
+++ workflow-cli/src/services/opensearch-indicesusage.service.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
+import {domainToASCII} from 'url';
 import AwsService from './aws.service';
 
 export interface settings {
@@ -17,22 +18,67 @@ export interface settings {
 
 export default class OpenSearchIndicesUsageService extends AwsService {
   public async getIndicesUsage(settings: settings): Promise<any> {
+    console.log('return from API call');
     const responseBody=await this.getTotalDocumentsOfIndices(settings);
     console.log(responseBody);
+    await this.getFieldsDocsOfIndices(responseBody, settings);
   }
+
+  private async getFieldsDocsOfIndices(resStr: any, settings: settings): Promise<any> {
+    return this.executeSignedHttpRequest({
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'host': settings.hostname,
+      },
+      data: JSON.stringify({
+        'size': 0,
+        'aggs': {
+          'response_codes': {
+            'terms': {
+              'field': settings.fieldname,
+              'size': 100,
+            },
+          },
+        },
+      }),
+      query: {
+        format: 'json',
+      },
+      hostname: settings.hostname,
+      path: `${settings.indicesname}/_search`,
+    })
+      .then((res) => this.waitAndReturnResponseBody(res))
+      .then((res) => {
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        console.log(res);
+        // return res;
+        console.log(`started`);
+      });
+  }
+
 
   private async getTotalDocumentsOfIndices(settings: settings): Promise<any> {
     // const timeStamp = this.getTimeStamp();
-    const res=await this.executeSignedHttpRequest({
+    return this.executeSignedHttpRequest({
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
         'host': settings.hostname,
       },
       hostname: settings.hostname,
-      path: `/_cat/indices/${settings.indicesname}?v&bytes=mb&h=index,docs.count`,
-    });
-    console.log(res);
+      path: `/_cat/indices/${settings.indicesname}`,
+      query: {
+        format: 'json',
+      },
+    })
+      .then((res) => this.waitAndReturnResponseBody(res))
+      .then((res) => {
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        // console.log(res);
+        return res;
+        console.log(`started`);
+      });
   }
 /*
   private getTimeStamp(): string {
