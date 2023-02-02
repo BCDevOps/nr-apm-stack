744d1dbc6dda4755707d95b85b5ece1d97d2c0b5
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2023-02-02 09:25:12 -0800

    feat: enable wildcard searching and update libraries

--- workflow-cli/src/commands/opensearch-index-usage.ts
+++ workflow-cli/src/commands/opensearch-index-usage.ts
@@ -3,8 +3,8 @@ import OpenSearchIndicesUsageService from '../services/opensearch-indicesusage.s
 
 const ACTION_SEARCH = '_search';
 
-export default class Indicesusage extends Command {
-  static description = 'Snapshot setup and creation tool';
+export default class OpensearchIndexUsage extends Command {
+  static description = 'Index usage generator tool';
 
   static examples = [
     '<%= config.bin %> <%= command.id %>',
@@ -27,13 +27,14 @@ export default class Indicesusage extends Command {
     accessKey: Flags.string({description: 'AWS secret access key', env: 'AWS_SECRET_ACCESS_KEY', required: true}),
     accountNumber: Flags.string({description: 'AWS account number', env: 'AWS_ACCOUNT_NUMBER', required: true}),
     arn: Flags.string({description: 'AWS ARN', env: 'AWS_ASSUME_ROLE'}),
-    indicesname: Flags.string({description: 'indices name', env: 'AWS_ASSUME_ROLE', required: true}),
-    fieldname: Flags.string({description: 'field name', env: 'AWS_ASSUME_ROLE', required: true}),
+    indicesname: Flags.string({description: 'indices name', env: 'OS_USAGE_INDICES', required: true}),
+    fieldname: Flags.string({
+      description: 'field name', env: 'OS_USAGE_FIELD', default: 'organization.id', required: true,
+    }),
   };
 
   public async run(): Promise<void> {
-    const {args} = await this.parse(Indicesusage);
-    const {flags} = await this.parse(Indicesusage);
+    const {args, flags} = await this.parse(OpensearchIndexUsage);
     const service = new OpenSearchIndicesUsageService();
     await service.assumeIdentity(flags);
     if (args.action === ACTION_SEARCH) {
