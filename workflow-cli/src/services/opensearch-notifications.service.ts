f92253227e06b76c330e1ecdee21d3eeac845c63
Andreas Wilson <41663792+andrwils@users.noreply.github.com>
2023-05-16 09:22:14 -0700

    Add notifications service/OneTeam priority channel (#278)

---
+++ workflow-cli/src/services/opensearch-notifications.service.ts
@@ -0,0 +1,50 @@
+/* eslint-disable @typescript-eslint/no-explicit-any, quotes */
+
+import AwsService from './aws.service';
+
+export interface settings {
+  hostname: string;
+  region: string;
+  accountNumber: string;
+  configId: string;
+  configIdName: string;
+  configNameLong: string;
+  configDescription: string;
+  configType: string;
+  isEnabled: boolean;
+  configTopicArnId: string;
+  configRoleArnId: string;
+}
+
+export default class OpenSearchNotificationsService extends AwsService {
+  public async createSnsChannel(settings: settings): Promise<any> {
+    return this.executeSignedHttpRequest({
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'host': settings.hostname,
+      },
+      hostname: settings.hostname,
+      path: `/_plugins/_notifications/configs/`,
+      body: {
+        "config_id": `${settings.configId}`,
+        "name": `${settings.configIdName}`,
+        "config": {
+          "name": `${settings.configNameLong}`,
+          "description": `${settings.configDescription}`,
+          "config_type": `${settings.configType}`,
+          "is_enabled": settings.isEnabled,
+          "sns": {
+            "topic_arn": `arn:aws:sns:${settings.region}:${settings.accountNumber}:${settings.configTopicArnId}`,
+            "role_arn": `arn:aws:iam::${settings.accountNumber}:role/${settings.configRoleArnId}`,
+          },
+        },
+      },
+    })
+      .then((res) => this.waitAndReturnResponseBody(res))
+      .then((res) => {
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        console.log(`Status code: [${res.statusCode}]`);
+      });
+  }
+}
