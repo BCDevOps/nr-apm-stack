f42805086d678ac93947ae177613ccded49ccec4
Andreas Wilson <41663792+andrwils@users.noreply.github.com>
2023-05-17 16:38:35 -0700

    Add assume identity

--- workflow-cli/src/commands/opensearch-notify-oneteam-priority.ts
+++ workflow-cli/src/commands/opensearch-notify-oneteam-priority.ts
@@ -10,6 +10,10 @@ export default class OpenSearchNotificationChannel extends Command {
 
   static flags = {
     hostname: Flags.string({char: 'u', description: 'OpenSearch url', env: 'OS_URL', required: true}),
+    domainName: Flags.string({char: 'd', description: 'OpenSearch Domain', env: 'OS_DOMAIN', required: true}),
+    accessId: Flags.string({description: 'AWS access key id', env: 'AWS_ACCESS_KEY_ID', required: true}),
+    accessKey: Flags.string({description: 'AWS secret access key', env: 'AWS_SECRET_ACCESS_KEY', required: true}),
+    arn: Flags.string({description: 'AWS ARN', env: 'AWS_ASSUME_ROLE'}),
     accountNumber: Flags.string({
       description: 'AWS account number', env: 'AWS_ACCOUNT_NUMBER', required: true}),
     region: Flags.string({
@@ -39,6 +43,7 @@ export default class OpenSearchNotificationChannel extends Command {
   public async run(): Promise<void> {
     const {flags} = await this.parse(OpenSearchNotificationChannel);
     const service = new OpenSearchNotificationsService();
+    await service.assumeIdentity(flags);
     await service.createSnsChannel(flags);
   }
 }
