93e95d7f764a9452b232141b555d89e0611c0057
Nisarg Vadgama <31422051+ngunner15@users.noreply.github.com>
2023-08-03 15:31:04 -0700

    Feat/add docs (#312)
    * feat: add documents with docsify
    
    * feat: add docs clarification and search (#305)
    
    * fix: add onboarding process
    
    * Add GitHub Action for docsify
    
    * Change the upload folder
    
    * Create a shell script to loop over files
    
    * Add shell script cmd
    
    * Add filename to save result
    
    ---------
    
    Co-authored-by: GraceRuan <grace.ruan@gov.bc.ca>
    Co-authored-by: Matthew Bystedt <mbystedt@gmail.com>

---
+++ .github/workflows/loop-script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+directory_path="./workflow-cli/configuration-opensearch/state_management_policy"
+
+# Loop over each file in the directory
+find "$directory_path" -type f -name "*.json" -print0 | while IFS= read -r -d $'\0' file; do
+  # Use jq to process the JSON data in the file
+  jq '.policy.description' > ./docs/commonschema.md
+done
