80fc836b8add5a9a36930246772fbd351c06d38c
Nisarg Vadgama <nisarg.vadgama@gov.bc.ca>
2023-08-08 09:55:57 -0700

    Remove -d flag

--- .github/workflows/loop-script.sh
+++ .github/workflows/loop-script.sh
@@ -3,7 +3,7 @@
 directory_path="./workflow-cli/configuration-opensearch/state_management_policy"
 
 # Loop over each file in the directory
-find "$directory_path" -type f -name "*.json" -print0 | while IFS= read -r -d $'\0' file; do
+find "$directory_path" -type f -name "*.json" -print0 | while IFS= read -r $'\0' file; do
   # Use jq to process the JSON data in the file
   jq '.policy.description' > ./docs/commonschema.md
 done
